2841d29a61b49ed62f9dbb1178c6bcb312f442ed
PeterRimon <Peter.rimon@genwin.net>
2021-10-21 17:48:28 +0200

    Login Happy Scenario

--- src/main/java/utility/Strings.java
+++ src/main/java/utility/Strings.java
@@ -2,4 +2,6 @@ package utility;
 
 public class Strings {
     public static final String REGISTER_URL = "https://demodashboard.genwin.app/register";
+    public static final String LOGIN_URL = "https://demodashboard.genwin.app/";
+    public static final String AdminPage_Dashboard = "https://demodashboard.genwin.app/admin";
 }
--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,2 +1,22 @@
+import activities.LoginImplementation;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Login {
+    LoginImplementation loginImplementation;
+
+    @BeforeClass
+    public void beforeRunning() {
+        loginImplementation = new LoginImplementation();
+    }
+
+    @Test
+    public void phase1Happyscenario() {
+        loginImplementation.fillHappyScenario();
+    }
+    @AfterTest
+    public void CloseBrowser() {
+    loginImplementation.TestEnd();
+    }
 }
